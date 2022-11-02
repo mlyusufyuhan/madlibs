7cb544627ab2e2f029c34189c349b7c6c7d16c59
yusufyuhan <115976807+mlyusufyuhan@users.noreply.github.com>
2022-11-02 19:10:49 +0530

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,7 @@
+adj = input("adjective:")
+verb1 = input("verb:")
+verb2 = input("verb:")
+famous_person = input("famous person:")
+
+madlib = f"comuter programming is so {adj}! it make me so exited all the time because  \
+I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}! "

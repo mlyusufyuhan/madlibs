b5f64f0638699a7536542ad28aeff3a5244a8346
yusufyuhan <115976807+mlyusufyuhan@users.noreply.github.com>
2023-02-27 22:04:09 +0530

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -5,3 +5,4 @@ famous_person = input("famous person:")
 
 madlib = f"comuter programming is so {adj}! it make me so exited all the time because  \
 I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}! "
+
